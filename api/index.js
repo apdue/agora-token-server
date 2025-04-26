d6f222ced8787d953d3b062f88bee12997dc3642
apdue <120237743+apdue@users.noreply.github.com>
2025-04-26 12:41:25 +0500

    Update index.js

--- api/index.js
+++ api/index.js
@@ -1,28 +1,28 @@
-const { RtcTokenBuilder, RtcRole } = require('agora-access-token');
+const RtcTokenBuilder = require("../src/RtcTokenBuilder2").RtcTokenBuilder;
+const RtcRole = require("../src/RtcTokenBuilder2").Role;
 
-module.exports = (req, res) => {
-  const appId = process.env.APP_ID;
-  const appCertificate = process.env.APP_CERTIFICATE;
-  const channelName = req.query.channel;
-  const uid = req.query.uid;
-  const role = RtcRole.PUBLISHER;
+// Get the value of the environment variable AGORA_APP_ID. Make sure you set this variable to the App ID you obtained from Agora console.
+const appId = process.env.AGORA_APP_ID;
+// Get the value of the environment variable AGORA_APP_CERTIFICATE. Make sure you set this variable to the App certificate you obtained from Agora console
+const appCertificate = process.env.AGORA_APP_CERTIFICATE;
+// Replace channelName with the name of the channel you want to join
+const channelName = "channelName";
+// Fill in your actual user ID
+const uid = 2882341273;
+// Set streaming permissions
+const role = RtcRole.PUBLISHER;
+// Token validity time in seconds
+const tokenExpirationInSecond = 3600;
+// The validity time of all permissions in seconds
+const privilegeExpirationInSecond = 3600;
 
-  if (!channelName || !uid) {
-    return res.status(400).json({ error: "channel and uid are required" });
-  }
+console.log("App Id:", appId);
+console.log("App Certificate:", appCertificate);
+if (appId == undefined || appId == "" || appCertificate == undefined || appCertificate == "") {
+  console.log("Need to set environment variable AGORA_APP_ID and AGORA_APP_CERTIFICATE");
+  process.exit(1);
+}
 
-  const expirationTimeInSeconds = 3600;
-  const currentTimestamp = Math.floor(Date.now() / 1000);
-  const privilegeExpiredTs = currentTimestamp + expirationTimeInSeconds;
-
-  const token = RtcTokenBuilder.buildTokenWithUid(
-    appId,
-    appCertificate,
-    channelName,
-    parseInt(uid),
-    role,
-    privilegeExpiredTs
-  );
-
-  return res.status(200).json({ token });
-};
+// Generate Token
+const tokenWithUid = RtcTokenBuilder.buildTokenWithUid(appId, appCertificate, channelName, uid, role, tokenExpirationInSecond, privilegeExpirationInSecond);
+console.log("Token with int uid:", tokenWithUid);
